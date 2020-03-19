62f7a4087565c26b88278f84a781b22485d357de
unknown <alighafoorzade@gmail.com>
2020-03-20 00:47:33 +0330

    Updates

---
+++ precache-manifest.03b592a992efd42f4f0cae7c8bf46d4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bf0f66419e61ecc654132dcf64fe58",
+    "url": "/maktabkhooneh-resume/index.html"
+  },
+  {
+    "revision": "8aba602895ea73943718",
+    "url": "/maktabkhooneh-resume/static/css/main.df80e859.chunk.css"
+  },
+  {
+    "revision": "c17cf05659662f95e1ec",
+    "url": "/maktabkhooneh-resume/static/js/2.d1434c1d.chunk.js"
+  },
+  {
+    "revision": "bd0c9fe1e58fc8dede9d02815061603b",
+    "url": "/maktabkhooneh-resume/static/js/2.d1434c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aba602895ea73943718",
+    "url": "/maktabkhooneh-resume/static/js/main.41ed9d6e.chunk.js"
+  },
+  {
+    "revision": "af3dbd942e9471c19144",
+    "url": "/maktabkhooneh-resume/static/js/runtime-main.5dd51774.js"
+  }
+]);
